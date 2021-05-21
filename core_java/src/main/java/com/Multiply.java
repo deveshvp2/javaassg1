4f805a69f8cb615ad47645f759e9b25245351906
Devesh <deveshvp2@gmail.com>
2021-05-21 16:08:10 +0530

    2nd commit

---
+++ core_java/src/main/java/com/Multiply.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Multiply {
+
+	
+		public static void main(String[] args) {
+
+	        int first = 5;
+	        int second = 3;
+
+	        int product = first * second;
+
+	        System.out.println("The product is: " + product);
+
+	}
+
+}
