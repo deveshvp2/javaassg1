e00af206a3e460a300ceeda842ec14e484042328
Devesh <deveshvp2@gmail.com>
2021-05-21 14:05:50 +0530

    my first commit

---
+++ core_java/src/main/java/com/Hello.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Hello {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello");
+
+//		int i = 5;
+//		int j = 10;
+//		int k = i+j;
+//		System.out.println("Result is: "+k);
+		
+		for(int i =1 ; i<=10 ; i++) {
+			System.out.println("Dev");
+		}
+		
+	}
+
+}
